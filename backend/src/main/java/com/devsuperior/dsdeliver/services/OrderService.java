8f374d9f8a70d9ac697ddb627398c0ebd5cdcba9
Alberto Paiva <59654091+mechztcc@users.noreply.github.com>
2021-01-07 17:29:54 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,11 +1,16 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,11 +22,28 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private ProductRepository ProductRepository;
+
     @Transactional(readOnly = true)
     public List<OrderDTO> findAll() {
         List<Order> list = repository.findOrdersWithProducts();
         //the content returned by stream() cannot be a list,
         // because of this, use Collectors.toList()
         return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+    }
+
+    // I think that i dont understand totally the part of line 43 - 46
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+        Instant.now(), OrderStatus.PENDING);
+
+        for(ProductDTO p : dto.getProducts()) {
+            Product product = ProductRepository.getOne(p.getId());
+            order.getProducts().add(product); 
+        }
+        order = repository.save(order);
+        return new OrderDTO(order);
     }
 }
